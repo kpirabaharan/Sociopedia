1cc06cbbd89191ef738f36d7f2e49f3a0e750629
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-02-12 18:05:51 -0500

    Social Media App: Styling Setup
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- social-media/client/src/App.js
+++ social-media/client/src/App.js
@@ -3,25 +3,34 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import HomePage from 'scenes/homePage';
 import LoginPage from 'scenes/loginPage';
 import ProfilePage from 'scenes/profilePage';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/system';
+import { themeSettings } from 'theme';
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   const router = createBrowserRouter([
     {
       path: '/',
       children: [
-        {
-          index: true,
-          element: <LoginPage />,
-          children: [
-            { path: 'home', element: <HomePage /> },
-            { path: 'profile:userId', element: <ProfilePage /> },
-          ],
-        },
+        { index: true, element: <LoginPage /> },
+        { path: 'home', element: <HomePage /> },
+        { path: 'profile:userId', element: <ProfilePage /> },
       ],
     },
   ]);
 
-  return <RouterProvider router={router} />;
+  return (
+    <RouterProvider router={router}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </RouterProvider>
+  );
 }
 
 export default App;
