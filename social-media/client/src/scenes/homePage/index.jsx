4281673ab5db2325d4ca2bbe88481797b7a0ade0
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-02-13 19:41:18 -0500

    Social Media App: Created User Profile Overview Widget
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- social-media/client/src/scenes/homePage/index.jsx
+++ social-media/client/src/scenes/homePage/index.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { useMediaQuery } from '@mui/material';
 import { Box } from '@mui/system';
 
 import NavBar from 'scenes/navBar';
+import UserWidget from 'scenes/widgets/UserWidget';
 
 const HomePage = () => {
+  // const user = useSelector((state) => state.user);
+  // console.log(user);
+
+  const isNonMobileScreens = useMediaQuery('(min-width:1000px)');
+  const { _id, picturePath } = useSelector((state) => state.user);
+
   return (
     <Box>
-      <NavBar></NavBar>
+      <NavBar />
+      <Box
+        width='100%'
+        p='2rem 6%'
+        display={isNonMobileScreens ? 'flex' : 'block'}
+        gap='0.5rem'
+        justifyContent='space-between'
+      >
+        <Box flexBasis={isNonMobileScreens ? '26%' : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? '42%' : undefined}
+          mt={isNonMobileScreens ? undefined : '2rem'}
+        ></Box>
+        {isNonMobileScreens && <Box flexBasis='26%'></Box>}
+      </Box>
     </Box>
   );
 };
