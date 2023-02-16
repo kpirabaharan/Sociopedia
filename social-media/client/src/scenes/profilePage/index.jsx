8a7bb37f9e4e3bae7386eafa596a1f87b406f34d
Keeshigan Pirabaharan <kpirabaharan3@gmail.com>
2023-02-15 22:00:18 -0500

    Social Media App: Created Profile Page
    Signed-off-by: Keeshigan Pirabaharan <kpirabaharan3@gmail.com>

--- social-media/client/src/scenes/profilePage/index.jsx
+++ social-media/client/src/scenes/profilePage/index.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { Box, useMediaQuery } from '@mui/material';
+
+import FriendListWidget from 'scenes/widgets/FriendListWidget';
+import MyPostWidget from 'scenes/widgets/MyPostWidget';
+import PostsWidget from 'scenes/widgets/PostsWidget';
+import UserWidget from 'scenes/widgets/UserWidget';
+import NavBar from 'scenes/navBar';
 
 const ProfilePage = () => {
-  return (
-    <div>ProfilePage</div>
-  )
-}
+  const [user, setUser] = useState(null);
+  const { userId } = useParams();
+  const token = useSelector((state) => state.token);
+  const isNonMobileScreens = useMediaQuery('(min-width:1000px)');
 
-export default ProfilePage
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:8080/users/${userId}`, {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    const data = await response.json();
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (!user) return null;
+
+  return (
+    <Box>
+      <NavBar />
+      <Box
+        width='100%'
+        p='2rem 6%'
+        display={isNonMobileScreens ? 'flex' : 'block'}
+        gap='2rem'
+        justifyContent='center'
+      >
+        <Box flexBasis={isNonMobileScreens ? '26%' : undefined}>
+          <UserWidget userId={userId} picturePath={user.picturePath} />
+          <Box m='2rem 0' />
+          <FriendListWidget userId={userId} isProfile={true} />
+        </Box>
+        <Box flexBasis={isNonMobileScreens ? '42%' : undefined}>
+          <PostsWidget userId={userId} isUserProfile={false} isProfile />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default ProfilePage;
